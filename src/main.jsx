c2c17ffa2ccbd9a2cc01eda7088d7d5d0308f871
ClaytonDewey <clay@dryadmedia.com>
2025-03-17 11:55:02 -0500

    chore: Import router & components

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+
+// react router
+import { createBrowserRouter, RouterProvider, Route } from 'react-router-dom';
+
+// local imports
+import Login from './components/Login';
+import Task from './components/Task';
+import Comments from './components/Comments';
+
+const router = createBrowserRouter([
+  { path: '/', element: <Login /> },
+  { path: '/task', element: <Task /> },
+  { path: '/comments/:category/:id', element: <Comment /> },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </StrictMode>
+);
