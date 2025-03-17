8ba9522ed381df412a59eb90cb4cb2d7680eb481
ClaytonDewey <clay@dryadmedia.com>
2025-03-17 12:47:04 -0500

    chore: Update components

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,9 +1,17 @@
+import AddTask from './AddTask';
 import Nav from './Nav';
+import TaskContainer from './TaskContainer';
+
+import io from 'socket.io-client';
+
+const socket = io.connect('http://localhost:3000');
 
 const Task = () => {
   return (
     <>
       <Nav />
+      <AddTask socket={socket} />
+      <TaskContainer socket={socket} />
     </>
   );
 };
--- src/components/TaskContainer.jsx
+++ src/components/TaskContainer.jsx
@@ -1,4 +1,33 @@
-const TaskContainer = () => {
-  return <div>TaskContainer</div>;
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+const TaskContainer = ({ socket }) => {
+  return (
+    <div className='container'>
+      <div className='pending__wrapper'>
+        <div className='pending__items'>
+          <p>Debug the Notification center</p>
+          <p className='comment'>
+            <Link to='/comments'>2 Comments</Link>
+          </p>
+        </div>
+      </div>
+      <div className='ongoing__wrapper'>
+        <div className='ongoing__items'>
+          <p>Ongoing Tasks</p>
+          <p className='comment'>
+            <Link to='/comments'>1 Comments</Link>
+          </p>
+        </div>
+      </div>
+      <div className='completed__wrapper'>
+        <div className='completed__items'>
+          <p>Completed Tasks</p>
+          <p className='comment'>
+            <Link to='/comments'>2 Comments</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default TaskContainer;
